8fa5bf53098ce6b10b1ab1f3e118d5d4b6e3cae5
Oleksii Dykan <alick_bass@mail.ru>
2017-03-16 14:26:16 +0100

    Project setup

---
+++ SweetRouter/SweetRouter.h
@@ -0,0 +1,19 @@
+//
+//  SweetRouter.h
+//  SweetRouter
+//
+//  Created by Oleksii on 16/03/2017.
+//  Copyright © 2017 ViolentOctopus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SweetRouter.
+FOUNDATION_EXPORT double SweetRouterVersionNumber;
+
+//! Project version string for SweetRouter.
+FOUNDATION_EXPORT const unsigned char SweetRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SweetRouter/PublicHeader.h>
+
+
