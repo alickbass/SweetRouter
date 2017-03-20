05cd7aa27de56b1f27cdc8d33c0983f9b59fbd58
Oleksii Dykan <alick_bass@mail.ru>
2017-03-20 10:26:02 +0100

    Make Environment URLRepresentable

--- SweetRouter/Router.swift
+++ SweetRouter/Router.swift
@@ -8,14 +8,24 @@
 
 import Foundation
 
-public protocol RouterType {
+public protocol URLRepresentable {
+    var components: URLComponents { get }
+    var url: URL { get }
+}
+
+public extension URLRepresentable {
+    public var url: URL {
+        guard let url = components.url else { fatalError("URL components are not valid") }
+        return url
+    }
+}
+
+public protocol RouterType: URLRepresentable {
     associatedtype Environment: EnvironmentType
     associatedtype Route: RouteType
     
     var environment: Environment { get }
     var route: Route { get }
-    var components: URLComponents { get }
-    var url: URL { get }
 }
 
 public protocol RouteType {
@@ -23,28 +33,31 @@ public protocol RouteType {
     var defaultPath: URL.Path { get }
 }
 
-public protocol EnvironmentType {
+public protocol EnvironmentType: URLRepresentable {
     var value: URL.Environment { get }
 }
 
-public extension RouterType {
-    var components: URLComponents {
+public extension EnvironmentType {
+    public var components: URLComponents {
         var components = URLComponents()
-        let environment = self.environment.value
-        let route = self.route
+        let environment = value
         
         components.scheme = environment.scheme.rawValue
         components.host = environment.host.hostString
         components.port = environment.port
+        
+        return components
+    }
+}
+
+public extension RouterType {
+    public var components: URLComponents {
+        var components = environment.components
+        
         components.path = route.defaultPath.with(route.route.path).pathValue
         components.queryItems = route.route.query?.queryItems
         
         return components
-    }
-    
-    var url: URL {
-        guard let url = components.url else { fatalError("URL components are not valid") }
-        return url
     }
 }
 
--- SweetRouterTests/TestEnvironment.swift
+++ SweetRouterTests/TestEnvironment.swift
@@ -10,11 +10,15 @@ import XCTest
 import SweetRouter
 
 class TestEnvironment: XCTestCase {
+    typealias Environment = URL.Environment
     
     func testEnvironmentEquatable() {
-        typealias Environment = URL.Environment
         XCTAssertEqual(Environment(IP(127, 0, 0, 1), 8080), Environment(.http, "127.0.0.1", 8080))
         XCTAssertEqual(Environment.localhost(4001), Environment(.http, "localhost", 4001))
     }
     
+    func testEnvironmentURLConvertable() {
+        XCTAssertEqual(Environment(IP(127, 0, 0, 1), 8080).url, URL(string: "http://127.0.0.1:8080"))
+        XCTAssertEqual(Environment.localhost(4001).url, URL(string: "http://localhost:4001"))
+    }
 }
