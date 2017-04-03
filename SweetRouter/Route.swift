54caa0fb89a455e742d6e0161e186886d4fbade6
Oleksii Dykan <alick_bass@mail.ru>
2017-04-03 10:16:18 +0200

    Add fragment and query methods on Route

--- SweetRouter/Route.swift
+++ SweetRouter/Route.swift
@@ -12,21 +12,36 @@ public extension URL {
     public struct Route {
         public let path: Path
         public let query: Query?
+        public let fragment: String?
         
         public init(path: Path, query: (name: String, value: QueryItemValue?)...) {
             self.path = path
             self.query = Query(query)
+            fragment = nil
+        }
+        
+        init(path: Path, query: Query?, fragment: String?) {
+            self.path = path
+            self.query = query
+            self.fragment = fragment
         }
         
         public init(path: Path) {
-            self.path = path
-            self.query = nil
+            self.init(path: path, query: nil, fragment: nil)
+        }
+        
+        public func query(_ query: (name: String, value: QueryItemValue?)...) -> Route {
+            return Route(path: path, query: Query(query), fragment: nil)
+        }
+        
+        public func fragment(_ fragment: String) -> Route {
+            return Route(path: path, query: query, fragment: fragment)
         }
     }
 }
 
 extension URL.Route: Equatable {
     public static func == (lhs: URL.Route, rhs: URL.Route) -> Bool {
-        return lhs.path == rhs.path && lhs.query == rhs.query
+        return lhs.path == rhs.path && lhs.query == rhs.query && lhs.fragment == rhs.fragment
     }
 }
--- SweetRouterTests/TestRoute.swift
+++ SweetRouterTests/TestRoute.swift
@@ -7,13 +7,18 @@
 //
 
 import XCTest
-import SweetRouter
+@testable import SweetRouter
 
 class TestRoute: XCTestCase {
     
     func testUrlRouteEquatable() {
         XCTAssertEqual(URL.Route(path: ["myPath"], query: ("user", nil), ("date", "12.04.02")), URL.Route(path: ["myPath"], query: ("user", nil), ("date", "12.04.02")))
         XCTAssertNotEqual(URL.Route(path: ["myPath"]), URL.Route(path: ["myPath"], query: ("user", nil)))
+    }
+    
+    func testUrlRouteQuery() {
+        XCTAssertEqual(URL.Route(path: ["myPath"]).query(("user", nil), ("date", "12.04.02")), URL.Route(path: ["myPath"], query: URL.Query(("user", nil), ("date", "12.04.02")), fragment: nil))
+        XCTAssertEqual(URL.Route(path: ["myPath"]).fragment("fragment"), URL.Route(path: ["myPath"], query: nil, fragment: "fragment"))
     }
     
 }
